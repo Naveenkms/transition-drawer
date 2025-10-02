a107ac65b8bc6f0841d5413b6da0240273664edd
Naveenkms <nvnkms123@gmail.com>
2025-10-02 18:23:54 +0530

    fix: lockfile mismatch with package.json

--- .source/index.ts
+++ .source/index.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1759397914185"
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1759409202080"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
 export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","fullPath":"content\\docs\\index.mdx"}, data: docs_0 }], [])
